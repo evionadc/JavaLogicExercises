60404f421059c933f6ba5a6005689abe7d914661
evionadc <36736967+evionadc@users.noreply.github.com>
2022-12-15 18:12:03 -0300

    first commit

---
+++ src/TaskOne.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TaskOne {
+	
+	private static boolean breaking = false;
+	private static String number;
+
+	public static void main(String[] args) {
+		
+		while(breaking == false) {
+			
+			breaking = getaNumber();
+		}
+		
+		
+		System.out.println("The combined value of " + number + " is " + sumDigits(getDigits(number)));
+	}
+	
+	@SuppressWarnings("resource")
+	private static boolean getaNumber()  {
+		
+		Scanner in = new Scanner( System.in );
+		System.out.println("Enter a number");
+		
+		number = in.next();
+		
+		try {
+			Integer.parseInt(number);
+			
+		} catch (Exception e) {
+			System.out.println("Enter only numbers");
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
+	public static List<Integer> getDigits(String number) {
+	   List<Integer> digits = new ArrayList<Integer>();
+	   for(int i = 0; i<number.length(); i++) {
+		   
+		   String y = String.valueOf(number.charAt(i));
+		   int x = Integer.parseInt(y);
+		   digits.add(x);
+	   }
+	   return digits;
+	}
+	
+	private static int sumDigits(List<Integer> digits) {
+		int total=0;
+		for (Integer digit : digits) {
+			total=total+digit;
+		}
+		
+		return total;
+	}
+
+}
---
+++ src/TaskTwo.java
@@ -0,0 +1,48 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class TaskTwo {
+	
+	static String number;
+	static boolean breaking;
+	static double pounds = 2.20;
+	static double onces = 35.27;
+
+	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("###,##0.00");
+		while(breaking == false) {
+			
+			breaking = getaNumber();
+		}
+		
+		int max = Integer.parseInt(number);
+		
+		System.out.println("| Kilogramas | Pounds | Onces |");
+		for(int i = 1 ; i<= max; i++) {
+			
+			System.out.println("|         " + (i)+ "  |" + df.format(i*pounds) + "    |" + df.format(i*onces) + " |" );
+			
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	private static boolean getaNumber()  {
+		
+		Scanner in = new Scanner( System.in );
+		System.out.println("Enter MAX");
+		
+		number = in.next();
+		
+		try {
+			Integer.parseInt(number);
+			
+		} catch (Exception e) {
+			System.out.println("Enter only numbers");
+			return false;
+		}
+		
+		return true;
+		
+	}
+
+}
